e3f0f557ce8e5273d5df895a37f7ed60d2c33d6e
Ian Guerrero <ian.guerrero.v@mail.pucv.cl>
2026-04-07 11:55:15 -0400

    Managers

---
+++ src/main/java/whatsapp/server/managers/Managers.java
@@ -0,0 +1,13 @@
+/*
+Clases para gestionar el estado global (ej. la lista de clientes conectados, los grupos activos).
+ */
+
+package whatsapp.server.managers;
+
+/**
+ *
+ * @author iangu
+ */
+public class Managers {
+    
+}
