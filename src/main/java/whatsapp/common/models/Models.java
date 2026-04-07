172824edf558d21970d02370eab0dc138c7d5355
Ian Guerrero <ian.guerrero.v@mail.pucv.cl>
2026-04-07 11:55:30 -0400

    Models

---
+++ src/main/java/whatsapp/common/models/Models.java
@@ -0,0 +1,15 @@
+/*
+Aquí irán las clases que implementen Serializable. Se exige el uso de 
+serialización o marshalling para el envío de estructuras de datos complejas. 
+En lugar de enviar texto plano, enviarán objetos (ej. PaqueteMensaje, PaqueteLlamada).
+ */
+
+package whatsapp.common.models;
+
+/**
+ *
+ * @author iangu
+ */
+public class Models {
+    
+}
